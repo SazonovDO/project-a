8f2791960318ff38c9118df603534466bb0bc41d
Dmitry Sazonov <sazonovdo@gmail.com>
2020-02-14 22:03:36 +0100

    Added deploy files added gitignore added requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from waitress import serve
+import argparse
 
 app = Flask(__name__)
 
@@ -9,4 +11,11 @@ def index_page():
 
 
 if __name__ == "__main__":
-    app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prod', help='Use waitress WSGI instead flask server', action='store_true')
+    args = parser.parse_args()
+
+    if args.prod:
+        serve(app, host='0.0.0.0', port=80)
+    else:
+        app.run()
